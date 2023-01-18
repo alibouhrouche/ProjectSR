bcb79bc5d3aba10247efae2832af069cc64e8305
Ali Bouhrouche <alibouhrouche@gmail.com>
2023-01-18 14:54:49 +0100

    Add edit client profile

--- src/main/java/com/app/controllers/ClientController.java
+++ src/main/java/com/app/controllers/ClientController.java
@@ -1,57 +1,33 @@
 package com.app.controllers;
 
 import com.app.entities.Client;
-import com.app.securityConfig.JwtService;
 import com.app.services.ClientServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/profile")
+@RequestMapping("/api/v1/client/profile")
 public class ClientController {
     private final PasswordEncoder passwordEncoder;
     private final ClientServices clientServices;
-    private final JwtService jwtService;
-    @GetMapping(produces = {"application/json", MediaType.ALL_VALUE}, consumes = MediaType.ALL_VALUE)
-    public ResponseEntity<Client> getClient(@RequestHeader("Authorization") String authorization) {
-        String email = jwtService.extractUsername(authorization.split(" ")[1]);
-        return ResponseEntity.ok(clientServices.getClient(email).get());
-    }
-    @PostMapping(value="save",produces = {"application/json", "application/xml"}, consumes = MediaType.ALL_VALUE)
-    public ResponseEntity<Client> saveClient(@RequestHeader("Authorization") String authorization,@RequestBody Client requestClient) {
-        String email = jwtService.extractUsername(authorization.split(" ")[1]);
-
-        Client client = clientServices.getClient(email).get();
-        client.setPrenom(requestClient.getPrenom());
-        client.setNom(requestClient.getNom());
-        client.setRue(requestClient.getRue());
-        client.setVille(requestClient.getVille());
-        client.setCodePostal(requestClient.getCodePostal());
-
-        if(!client.getEmail().equals(requestClient.getEmail())){
-            Optional<Client> client1 = clientServices.getClient(requestClient.getEmail());
-            if(client1.isPresent()){
-                throw new IllegalStateException("This Email is taken!");
-            }
-            client.setEmail(requestClient.getEmail());
+    @PutMapping(produces = {"application/json", "application/xml"}, consumes = MediaType.ALL_VALUE)
+    public ResponseEntity<Client> updateClient(Authentication authentication,@RequestBody Client client) {
+        Client c = (Client)authentication.getPrincipal();
+        if(client.getPassword() != null && !client.getPassword().isEmpty()) {
+            c.setPassword(passwordEncoder.encode(client.getPassword()));
         }
-        if(!requestClient.getPassword().equals("") && !requestClient.getPassword().isEmpty()){
-            client.setPassword(passwordEncoder.encode(requestClient.getPassword()));
-        }
-        return ResponseEntity.ok(clientServices.saveClient(client));
-    }
-    @PutMapping(value="update",produces = {"application/json", "text/json"}, consumes = MediaType.ALL_VALUE)
-    public ResponseEntity<Client> updateClient(@RequestBody Client client) {
-        return ResponseEntity.ok(clientServices.saveClient(client));
-    }
-    @DeleteMapping("delete/{id}")
-    public ResponseEntity<Client> deleteClient(@PathVariable Integer id){
-        return ResponseEntity.ok(clientServices.delete(id));
+        c.setPrenom(client.getPrenom());
+        c.setNom(client.getNom());
+        c.setEmail(client.getEmail());
+        c.setRue(client.getRue());
+        c.setVille(client.getVille());
+        c.setCodePostal(client.getCodePostal());
+        return ResponseEntity.ok(clientServices.saveClient(c));
     }
 }
